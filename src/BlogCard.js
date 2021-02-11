513f2daa79fd49939e3cd24a3809d0bd77492518
RajkumarDarshanala <55500896+RajkumarDarshanala@users.noreply.github.com>
2021-02-11 15:18:27 +0530

    error-free Blogs-style-version-2 Link tag fixed to image and description only  completed

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -66,33 +66,34 @@ function BlogCard(props) {
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card className={classes.card}>
-        <CardActionArea>
-          <Link
-            // to={`/postPage/${blog.id}`}
-            to={{
-              pathname: "./postPage",
-              state: {
-                blog: blog,
-              },
-            }}
-            style={{ textDecoration: "none" }}
-          >
+        <Link
+          // to={`/postPage/${blog.id}`}
+          to={{
+            pathname: "./postPage",
+            state: {
+              blog: blog,
+            },
+          }}
+          style={{ textDecoration: "none" }}
+        >
+          <CardActionArea>
             <CardMedia
               className={classes.media}
               image="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
               title="Contemplative Reptile"
             />
-          </Link>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              React useContext
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                React useContext
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Link>
         <CardActions className={classes.cardActions}>
           <Box className={classes.author}>
             <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
